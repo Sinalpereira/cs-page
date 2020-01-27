a986cfe9fa4975eb00344ca19bbda0447fa8d3e6
Sinalpereira <sinal.pereira@contentstack.com>
2020-01-27 18:12:11 +0530

    added media query

--- gulpFile.js
+++ gulpFile.js
@@ -1,14 +1,21 @@
 var gulp=require('gulp');
+var path = require('path');
 var less=require('gulp-less');
 var postcss=require('gulp-postcss');
 var gcmq = require('gulp-group-css-media-queries');
 var cmq=require('gulp-combine-media-queries');
 var autoprefixer=require('autoprefixer');
 var cssnano=require('cssnano');
+var pcmq = require ('postcss-extract-media-query');
 
 // -------------------css task-------------------//
 gulp.task("css",function(){
-  var plugins=[cssnano(),autoprefixer()];
+  var plugins=[cssnano(),autoprefixer(),pcmq({
+    output : {
+      path : path.join('src/postcss','postcss'),
+      name: '[name]-[query].[ext]'
+    }
+  })];
     return gulp.src('src/less/index.less')
     .pipe(less())
     .pipe(gulp.dest('src/dist'))
